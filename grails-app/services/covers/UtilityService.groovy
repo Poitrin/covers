603f9d29e9ee71055138f4ca5a9be981bb1760d2
Poitrin <dev@poitrin.com>
2019-01-20 19:29:19 +0100

    Read Header from Proxy (nginx), before using getRemoteAddr

--- grails-app/services/covers/UtilityService.groovy
+++ grails-app/services/covers/UtilityService.groovy
@@ -6,6 +6,13 @@ class UtilityService {
   String getCurrentUserIpAddressHash(request) {
     // TODO: truncate IP address?
     // input.substring(0, input.lastIndexOf('.'))
-    return request.getRemoteAddr().hashCode().toString();
+
+    // Nginx setzt einen zusätzlichen Header, mit dem wir die "richtige" remoteAddr herausbekommen
+    String ipAddress = request.getHeader("X-Forwarded-For");
+    if (ipAddress == null) {
+      // Direkter Zugriff ohne Proxy
+      ipAddress = request.getRemoteAddr();
+    }
+    return ipAddress.hashCode().toString();
   }
 }
