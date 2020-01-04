dc8cccd2e0d427e6daa22526d2d9b19b54f888ca
Poitrin <dev@poitrin.com>
2020-01-04 11:44:38 +0100

    Upgrade to Grails 4

--- grails-app/migrations/20190210.groovy
+++ grails-app/migrations/20190210.groovy
@@ -7,16 +7,4 @@ databaseChangeLog = {
             }
         }
     }
-
-    changeSet(author: "gradle (generated)", id: "1549806781244-2") {
-        addDefaultValue(columnDataType: "bigint", columnName: "id", defaultValueComputed: "nextval('creative_work_id_seq'::regclass)", tableName: "creative_work")
-    }
-
-    changeSet(author: "gradle (generated)", id: "1549806781244-3") {
-        addDefaultValue(columnDataType: "bigint", columnName: "id", defaultValueComputed: "nextval('part_id_seq'::regclass)", tableName: "part")
-    }
-
-    changeSet(author: "gradle (generated)", id: "1549806781244-4") {
-        addDefaultValue(columnDataType: "bigint", columnName: "id", defaultValueComputed: "nextval('suggestion_id_seq'::regclass)", tableName: "suggestion")
-    }
 }
